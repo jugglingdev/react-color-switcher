0c7e94fdc38abfc05ab465499c453152a2b6b60b
jugglingdev <kaylamarie1785@gmail.com>
2024-01-25 14:28:12 -0600

    Set up heading and buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='react-root'>
+      <div className='centered'>
+        <h1>Color Picker</h1>
+        <button className='red'>red</button>
+        <button className='blue'>blue</button>
+        <button className='yellow'>yellow</button>
+      </div>
     </div>
   );
 }
