b2843a9884a4ab911e96f2af8347a01deb24ae48
jugglingdev <kaylamarie1785@gmail.com>
2024-01-25 14:36:16 -0600

    Add state to change background on button click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('')
+  console.log(color, setColor)
   return (
-    <div className='react-root'>
+    <div className={'react-root ' + color}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <button className='red'>red</button>
-        <button className='blue'>blue</button>
-        <button className='yellow'>yellow</button>
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
       </div>
     </div>
   );
